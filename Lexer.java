86f42de958031d381270f9799473815319b443f8
Aubrey <60026978+au6rey@users.noreply.github.com>
2022-02-20 10:36:14 -0600

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -1,3 +1,6 @@
+//Aubrey Oyoolo
+//Bruce Quach
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -5,7 +8,7 @@ import java.util.HashMap;
 
 enum Token {
 	IF, FOR, WHILE, FUNCTION, RETURN, INT, ELSE, DO, BREAK, END, ASSIGN, ADD, SUB, MUL, DIV, MOD, GT, LT, GE, LE, LP,
-	RP, IDENT, STRING, INT_LIT, FLOAT, INVALID, SEMI, INC, LB, RB
+	RP, IDENT, STRING, INT_LIT, SEMI, INC, LB, RB, EE, COMMA, NEG, OR, AND
 }
 
 public class Lexer {
@@ -15,15 +18,17 @@ public class Lexer {
 	public static void Tokenize(String filePath) {
 		initializeTokens();
 		try {
-			File file = new File(filePath);
-			FileReader fr;
-			fr = new FileReader(file);
+			File inputfile = new File(filePath);
+			FileReader fr = new FileReader(inputfile);
 			BufferedReader br = new BufferedReader(fr);
+
 			String line;
 			while ((line = br.readLine()) != null) {
 				analyzeCodeLine(line.strip());
 			}
+
 			br.close();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -32,7 +37,6 @@ public class Lexer {
 	private static void initializeTokens() {
 
 		try {
-
 			wordsAndOperatorsDictionary.put("if", Token.IF);
 			wordsAndOperatorsDictionary.put("for", Token.FOR);
 			wordsAndOperatorsDictionary.put("while", Token.WHILE);
@@ -45,7 +49,6 @@ public class Lexer {
 			wordsAndOperatorsDictionary.put("end", Token.END);
 			wordsAndOperatorsDictionary.put("=", Token.ASSIGN);
 			wordsAndOperatorsDictionary.put("+", Token.ADD);
-			wordsAndOperatorsDictionary.put("++", Token.INC);
 			wordsAndOperatorsDictionary.put("-", Token.SUB);
 			wordsAndOperatorsDictionary.put("*", Token.MUL);
 			wordsAndOperatorsDictionary.put("/", Token.DIV);
@@ -54,10 +57,16 @@ public class Lexer {
 			wordsAndOperatorsDictionary.put("<", Token.LT);
 			wordsAndOperatorsDictionary.put(">=", Token.GE);
 			wordsAndOperatorsDictionary.put("<=", Token.LE);
+			wordsAndOperatorsDictionary.put("++", Token.INC);
 			wordsAndOperatorsDictionary.put("(", Token.LP);
 			wordsAndOperatorsDictionary.put(")", Token.RP);
 			wordsAndOperatorsDictionary.put("{", Token.LB);
 			wordsAndOperatorsDictionary.put("}", Token.RB);
+			wordsAndOperatorsDictionary.put("|", Token.OR);
+			wordsAndOperatorsDictionary.put("&", Token.AND);
+			wordsAndOperatorsDictionary.put("==", Token.EE);
+			wordsAndOperatorsDictionary.put("!", Token.NEG);
+			wordsAndOperatorsDictionary.put(",", Token.COMMA);
 			wordsAndOperatorsDictionary.put(";", Token.SEMI);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -69,24 +78,27 @@ public class Lexer {
 			String[] lineArr = line.split(" ");
 			for (String str : lineArr) {
 				Token token = null;
+				
 				if (wordsAndOperatorsDictionary.containsKey(str.toLowerCase())) {
 					token = wordsAndOperatorsDictionary.get(str.toLowerCase());
 					System.out.println(token);
 				}
 
 				else
-					evaluate(str, token);
+					evaluate(str);
 			}
 		}
 
 	}
 
-	private static void evaluate(String str, Token previousToken) throws Exception {
+	private static void evaluate(String str) throws Exception {
 		String word = "";
 		String operator = "";
 		char[] chars = str.toCharArray();
+		
 		if (chars.length > 1) {
 			char nextChar;
+			
 			if ((Character.isLetter(chars[0]) || Character.isDigit(chars[0]))) {
 				word += chars[0];
 			} else {
@@ -96,13 +108,21 @@ public class Lexer {
 			for (int i = 1; i < chars.length; i++) {
 				nextChar = chars[i];
 				boolean isLastIndex = i == chars.length - 1;
-				boolean canCheckLongOperator = i <= chars.length - 1;
-				boolean currentOperatorHasMoreThanTwoSymbols = (((nextChar == '<' || nextChar == '>')
-						&& chars[i + 1] == '=') || (nextChar == '+' && chars[i + 1] == '+'));
+				boolean currentOperatorHasMoreThanOneSymbol = false;
+
+				if (!isLastIndex) {
+					boolean canCheckLongOperator = i + 1 <= chars.length - 1;
+					boolean isComparisonOperator = (nextChar == '<' || nextChar == '>' || nextChar == '=');
+					
+					currentOperatorHasMoreThanOneSymbol = canCheckLongOperator
+							&& ((isComparisonOperator && chars[i + 1] == '=')
+									|| (nextChar == '+' && chars[i + 1] == '+'));
+				}
+
 				if ((Character.isLetter(nextChar) || Character.isDigit(nextChar))) {
 					word += nextChar;
 
-					if (canCheckLongOperator && currentOperatorHasMoreThanTwoSymbols) {
+					if (currentOperatorHasMoreThanOneSymbol) {
 						operator = nextChar + "" + chars[i + 1];
 						i += 1;
 					}
@@ -113,18 +133,18 @@ public class Lexer {
 					if (!isValidOperator)
 						handleSyntaxError();
 
-					boolean isValidIdent = false;
 					if (isLastIndex) {
-						isValidIdent = analyzeWordAndDetermineIfItIsValid(word);
+						boolean isValidIdent = analyzeWordAndDetermineIfItIsValid(word);
 						word = "";
-						if (!isValidIdent) {
+
+						if (!isValidIdent)
 							handleSyntaxError();
-						}
 					}
 
 				} else {
 					operator = "" + nextChar;
-					if (canCheckLongOperator && currentOperatorHasMoreThanTwoSymbols) {
+
+					if (currentOperatorHasMoreThanOneSymbol) {
 						operator = nextChar + "" + chars[i + 1];
 						i += 1;
 					}
@@ -143,12 +163,16 @@ public class Lexer {
 				}
 			}
 		} else {
-			String word2 = chars[0] + "";
-			boolean isValidIdent = analyzeWordAndDetermineIfItIsValid(word2);
-			if (!isValidIdent) {
-				analyzeOperatorAndDetermineIfItIsValid(word2);
-			}
-			word2 = "";
+			String character = chars[0] + "";
+			boolean isValid = false;
+			
+			if(Character.isLetter(chars[0]) || Character.isLetter(chars[0])) {
+				isValid = analyzeWordAndDetermineIfItIsValid(character);
+			}else
+				isValid =  analyzeOperatorAndDetermineIfItIsValid(character);
+			
+			if (!isValid)
+				handleSyntaxError();
 		}
 	}
 
@@ -156,42 +180,57 @@ public class Lexer {
 		throw new Exception("SYNTAX ERROR");
 	}
 
-	private static boolean analyzeWordAndDetermineIfItIsValid(String word) {
+	private static boolean analyzeWordAndDetermineIfItIsValid(String word) throws Exception {
+
+		boolean isValid = true;
 		if (!word.isBlank()) {
+			String outputLine = "";
+
 			if (wordsAndOperatorsDictionary.containsKey(word.toLowerCase())) {
 				Token token = wordsAndOperatorsDictionary.get(word.toLowerCase());
-				System.out.println(token);
+				outputLine = token + "";
 			} else {
+
 				if (Character.isLetter(word.charAt(0))) {
-					System.out.println(Token.IDENT + ": " + word);
+					outputLine = Token.IDENT + ": " + word;
 				}
+
 				if (Character.isDigit(word.charAt(0))) {
+
 					if (word.matches("^[0-9]*$")) {
-						System.out.println(Token.INT_LIT + ": " + word);
+						outputLine = Token.INT_LIT + ": " + word;
 					} else {
-						System.out.println("SYNTAX ERROR: INVALID IDENTIFIER NAME: " + word);
-						return false;
+						outputLine = "SYNTAX ERROR: INVALID IDENTIFIER NAME: " + word;
+						isValid = false;
 					}
 				}
 			}
-			word = "";
+			System.out.println(outputLine);
 		}
-		return true;
+
+		return isValid;
 	}
 
-	private static boolean analyzeOperatorAndDetermineIfItIsValid(String operator) {
+	private static boolean analyzeOperatorAndDetermineIfItIsValid(String operator) throws Exception {
+
+		boolean isValid = true;
 		if (!operator.isBlank()) {
+			String outputLine = "";
+
 			if (wordsAndOperatorsDictionary.containsKey(operator.toLowerCase())) {
 				Token token = wordsAndOperatorsDictionary.get(operator.toLowerCase());
-				System.out.println(token);
+				outputLine = token + "";
+
 			} else {
-				System.out.println("INVALID OPERATOR");
-				return false;
+				outputLine = "INVALID OPERATOR: " + operator;
+				isValid = false;
 			}
-			operator = "";
+
+			System.out.println(outputLine);
+
 		}
 
-		return true;
+		return isValid;
 	}
 
 }
